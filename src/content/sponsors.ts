1f677e3bd24eebccb6fc30e83df42a7fec6ae559
Troy DeSpain <despaintroy@gmail.com>
2022-03-26 16:33:57 -0600

    Sponsorship where money is spent

--- src/content/sponsors.ts
+++ src/content/sponsors.ts
@@ -1,3 +1,39 @@
+import { Award, Bolt, Icon, Pizza, Shirt } from 'tabler-icons-react';
+
+export interface CategorySpend {
+  name: string;
+  amount: number;
+  color: string;
+  icon: Icon;
+}
+
+export const categorySpending: CategorySpend[] = [
+  {
+    name: 'Food',
+    amount: 11359,
+    color: 'red',
+    icon: Pizza,
+  },
+  {
+    name: 'T-shirts & Swag',
+    amount: 3640,
+    color: 'indigo',
+    icon: Shirt,
+  },
+  {
+    name: 'Prizes',
+    amount: 3826,
+    color: 'yellow',
+    icon: Award,
+  },
+  {
+    name: 'Misc',
+    amount: 798,
+    color: 'green',
+    icon: Bolt,
+  },
+];
+
 export interface Sponsor {
   name: string;
   logo?: string;
--- src/pages/sponsor.tsx
+++ src/pages/sponsor.tsx
@@ -1,6 +1,19 @@
-import { Container, createStyles, Group, Image, Title, useMantineTheme } from '@mantine/core';
-import { currentSponsors } from 'content/sponsors';
+import {
+  Box,
+  Container,
+  createStyles,
+  Group,
+  Image,
+  List,
+  RingProgress,
+  Text,
+  ThemeIcon,
+  Title,
+  useMantineTheme,
+} from '@mantine/core';
+import { categorySpending, currentSponsors } from 'content/sponsors';
 import React from 'react';
+import { CircleCheck } from 'tabler-icons-react';
 import Layout from '../components/Layout/Layout';
 
 const useStyles = createStyles((theme) => ({
@@ -19,11 +32,66 @@ const useStyles = createStyles((theme) => ({
 export default function Sponsor() {
   const { classes } = useStyles();
   const theme = useMantineTheme();
-  console.log(currentSponsors);
+
+  const totalSpend = categorySpending.reduce((acc, curr) => acc + curr.amount, 0);
+
+  console.log(totalSpend);
+
   return (
     <Layout>
       <Container>
-        <Title className={classes.title}>CURRENT SPONSORS</Title>
+        <Title order={1} className={classes.title}>
+          SPONSOR HACKUSU
+        </Title>
+
+        <Title order={2} className={classes.title}>
+          HOW SPONSORSHIP HELPS
+        </Title>
+        <Group position='center' mb={50}>
+          <Box>
+            <RingProgress
+              size={300}
+              thickness={40}
+              label={
+                <Text size="xs" align="center">
+                  2022 Budget Allocation
+                </Text>
+              }
+              sections={categorySpending.map((category) => ({
+                color: category.color,
+                value: (100 * category.amount) / totalSpend,
+              }))}
+            />
+          </Box>
+          <Box>
+            <List
+              spacing="xs"
+              size="sm"
+              center
+              icon={
+                <ThemeIcon color="teal" size={24} radius="xl">
+                  <CircleCheck size={16} />
+                </ThemeIcon>
+              }
+            >
+              {categorySpending.map((category) => (
+                <List.Item
+                  icon={
+                    <ThemeIcon color={category.color} size={24} radius="xl">
+                      <category.icon size={16} />
+                    </ThemeIcon>
+                  }
+                >
+                  {category.name}
+                </List.Item>
+              ))}
+            </List>
+          </Box>
+        </Group>
+
+        <Title order={2} className={classes.title}>
+          CURRENT SPONSORS
+        </Title>
         <Group position="center" spacing={theme.spacing.xl * 2} mt={50}>
           {currentSponsors
             .filter((sponsor) => sponsor.logo)
