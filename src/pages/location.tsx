b49b2eeae1831142a18b0e8d60ee370072ab5ba4
Troy DeSpain <despaintroy@gmail.com>
2022-03-27 10:12:50 -0600

    Add map to location page

--- src/pages/location.tsx
+++ src/pages/location.tsx
@@ -1,4 +1,4 @@
-import { Container, Title } from '@mantine/core';
+import { Container, Paper, Title } from '@mantine/core';
 import React from 'react';
 import Layout from '../components/Layout/Layout';
 
@@ -6,7 +6,18 @@ export default function Location() {
   return (
     <Layout>
       <Container>
-        <Title align='center'>LOCATION</Title>
+        <Title align="center">LOCATION</Title>
+        <Paper shadow="sm" withBorder mt="xl" p="0">
+          <iframe
+            style={{ border: 0 }}
+            src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d2977.0787242121787!2d-111.81117427366463!3d41.7403948072429!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xc88fd9fc64697ebb!2sUSU%20Huntsman%20Hall%20Building%20(HH)!5e0!3m2!1sen!2sus!4v1648397284322!5m2!1sen!2sus"
+            width="100%"
+            height={300}
+            frameBorder="0"
+            allowFullScreen={true}
+            className="mb-2"
+          />
+        </Paper>
       </Container>
     </Layout>
   );
