5fed91e00a0065fcbdbad450fd30035a15125645
Troy DeSpain <despaintroy@gmail.com>
2022-06-13 16:24:00 -0400

    Add illustration page

--- src/components/Press/Attending.tsx
+++ src/components/Press/Attending.tsx
@@ -1,4 +1,4 @@
-import { Anchor, Text, Title } from '@mantine/core';
+import { Anchor, Grid, Image, Text, Title } from '@mantine/core';
 import Section from 'components/Layout/Section';
 import React, { FC } from 'react';
 import { HACKUSU_EMAIL } from 'utils/constants';
@@ -6,32 +6,44 @@ import { HACKUSU_EMAIL } from 'utils/constants';
 const Attending: FC = () => {
   return (
     <Section title="Attending HackUSU for Media" background="light">
-      <Title order={3} mt={30}>
-        Registration
-      </Title>
-      <Text mt={10}>
-        Email us at{' '}
-        <Anchor href={`mailto:${HACKUSU_EMAIL}`} target="_blank">
-          {HACKUSU_EMAIL}
-        </Anchor>{' '}
-        if you plan on attending HackUSU so we can add you to the guest list.
-      </Text>
+      <Grid align="center" justify="center">
+        <Grid.Col sm={6} md={5}>
+          <Image
+            src={require('images/illustrations/location-pin.svg').default}
+            alt="Location pin illustration"
+            sx={{ maxWidth: 500, marginInline: 'auto' }}
+          />
+        </Grid.Col>
+        <Grid.Col sm={6} md={7}>
+          <Title order={3} mt={30}>
+            Registration
+          </Title>
+          <Text mt={10}>
+            Email us at{' '}
+            <Anchor href={`mailto:${HACKUSU_EMAIL}`} target="_blank">
+              {HACKUSU_EMAIL}
+            </Anchor>{' '}
+            if you plan on attending HackUSU so we can add you to the guest list.
+          </Text>
 
-      <Title order={3} mt={30}>
-        Parking and Check-in
-      </Title>
-      <Text mt={10}>
-        Members of the media should contact us at{' '}
-        <Anchor href={`mailto:${HACKUSU_EMAIL}`} target="_blank">
-          {HACKUSU_EMAIL}
-        </Anchor>{' '}
-        to arrange parking on campus. We will coordinate with the USU Parking Office to provide any
-        necessary permits for free.
-      </Text>
-      <Text mt={10}>
-        After arriving, please check-in at the north entrance of Huntsman Hall to pick up your
-        badge. You are welcome to attend all portions of the event and join us for any of the meals.
-      </Text>
+          <Title order={3} mt={30}>
+            Parking and Check-in
+          </Title>
+          <Text mt={10}>
+            Members of the media should contact us at{' '}
+            <Anchor href={`mailto:${HACKUSU_EMAIL}`} target="_blank">
+              {HACKUSU_EMAIL}
+            </Anchor>{' '}
+            to arrange parking on campus. We will coordinate with the USU Parking Office to provide
+            any necessary permits for free.
+          </Text>
+          <Text mt={10}>
+            After arriving, please check-in at the north entrance of Huntsman Hall to pick up your
+            badge. You are welcome to attend all portions of the event and join us for any of the
+            meals.
+          </Text>
+        </Grid.Col>
+      </Grid>
     </Section>
   );
 };
