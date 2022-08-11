9f5a916f83db4c3e82e119431f823bd1c4ec71ed
Troy DeSpain <despaintroy@gmail.com>
2022-08-10 21:00:20 -0600

    Update sponsor content

--- src/content/sponsor/sponsorLevelsContent.ts
+++ src/content/sponsor/sponsorLevelsContent.ts
@@ -1,19 +1,19 @@
 export enum LevelName {
-  BRONZE = 'Bronze',
-  SILVER = 'Silver',
-  GOLD = 'Gold',
+  AFFILIATE = 'Affiliate',
   PARTNER = 'Partner',
+  TRACK = 'Track',
+  TITLE = 'Title',
 }
 
-export const levelData: Record<LevelName, { amount: number; color: string }> = {
+export const levelData: Record<LevelName, { amount: number; color: string; limit?: number }> = {
   // [LevelName.BRONZE]: { amount: 500, color: 'linear-gradient(280deg, rgba(141,99,87,1) 0%, rgba(162,119,107,1) 100%)' },
   // [LevelName.SILVER]: { amount: 1500, color: 'linear-gradient(280deg, rgba(144,144,160,1) 0%, rgba(163,163,179,1) 100%)' },
   // [LevelName.GOLD]: { amount: 5000, color: 'linear-gradient(280deg, rgba(240,173,78,1) 0%, rgba(255,220,145,1) 100%)' },
   // [LevelName.PARTNER]: { amount: 8000, color: 'linear-gradient(280deg, rgba(71,129,228,1) 0%, rgba(111,175,231,1) 100%)' },
-  [LevelName.BRONZE]: { amount: 500, color: '#9D7367' },
-  [LevelName.SILVER]: { amount: 1500, color: '#9090A0' },
-  [LevelName.GOLD]: { amount: 5000, color: '#f0ad4e' },
-  [LevelName.PARTNER]: { amount: 8000, color: '#4781E4' },
+  [LevelName.AFFILIATE]: { amount: 500, color: '#9D7367' },
+  [LevelName.PARTNER]: { amount: 1000, color: '#9090A0' },
+  [LevelName.TRACK]: { amount: 5000, color: '#f0ad4e', limit: 4 },
+  [LevelName.TITLE]: { amount: 20000, color: '#4781E4', limit: 1 },
 };
 
 // Returns a list of the levels above the given level, including the level
@@ -42,22 +42,18 @@ export const sponsorLevelTableData: BenefitGroup[] = [
     benefits: [
       {
         name: 'Send engineers and mentors',
-        levels: levelsAbove(LevelName.BRONZE),
-      },
-      {
-        name: 'Help with judging',
-        levels: levelsAbove(LevelName.BRONZE),
+        levels: levelsAbove(LevelName.PARTNER),
       },
       {
         name: 'Host workshops',
-        levels: levelsAbove(LevelName.BRONZE),
+        levels: levelsAbove(LevelName.PARTNER),
         tooltip:
-          'Workshops do not promote any specifc product or service, but instead focus on generally applicable skills',
+          'Workshops may not promote your product or service, but instead focus on generally applicable skills',
       },
       {
-        name: 'Promotional tech-talks',
-        levels: levelsAbove(LevelName.GOLD),
-        tooltip: 'Promotional tech-talks allow you to promote your company, products, or services.',
+        name: 'Company info-session',
+        levels: levelsAbove(LevelName.TRACK),
+        tooltip: 'Info-sessions allow you to promote your company, products, or services.',
       },
     ],
   },
@@ -65,51 +61,58 @@ export const sponsorLevelTableData: BenefitGroup[] = [
     groupName: 'Recruiting',
     benefits: [
       {
-        name: 'Recruiting table',
-        levels: levelsAbove(LevelName.BRONZE),
-        levelsDetails: {
-          [LevelName.BRONZE]: 'Sat. Afternoon Only',
-        },
+        name: 'Resume Access',
+        levels: levelsAbove(LevelName.AFFILIATE),
       },
       {
-        name: 'Interview room',
-        levels: levelsAbove(LevelName.PARTNER),
+        name: 'Recruiting table',
+        levels: levelsAbove(LevelName.AFFILIATE),
+        levelsDetails: {
+          [LevelName.AFFILIATE]: 'Recruiting Event Only',
+        },
       },
+      // {
+      //   name: 'Interview room',
+      //   levels: levelsAbove(LevelName.TITLE),
+      // },
     ],
   },
   {
     groupName: 'Branding',
     benefits: [
       {
-        name: 'Logo on t-shirts and website',
-        levels: levelsAbove(LevelName.SILVER),
+        name: 'Logo on website',
+        levels: levelsAbove(LevelName.AFFILIATE),
+      },
+      {
+        name: 'Logo on event t-shirts',
+        levels: levelsAbove(LevelName.PARTNER),
         levelsDetails: {
-          [LevelName.SILVER]: 'Small',
-          [LevelName.GOLD]: 'Medium',
-          [LevelName.PARTNER]: 'Large',
+          [LevelName.PARTNER]: 'Small',
+          [LevelName.TRACK]: 'Medium',
+          [LevelName.TITLE]: 'Large',
         },
         tooltip:
           'Your sponsorship must be confirmed at least 6 weeks prior to the event to include your logo on t-shirts',
       },
       {
         name: 'Your swag included in swag bags',
-        levels: levelsAbove(LevelName.GOLD),
+        levels: levelsAbove(LevelName.TRACK),
       },
       {
         name: 'Sponsored competition category',
-        levels: levelsAbove(LevelName.GOLD),
+        levels: levelsAbove(LevelName.TRACK),
+        tooltip:
+          'Track sponsors may each name and sponsor one competition category, including providing judges',
       },
       {
-        name: 'Opening keynote presentation',
-        levels: levelsAbove(LevelName.PARTNER),
-      },
-      {
-        name: 'Award ceremony presentation',
-        levels: levelsAbove(LevelName.PARTNER),
+        name: 'Keynote presentation',
+        levels: levelsAbove(LevelName.TITLE),
       },
       {
         name: 'Prominent company branding at check-in, keynote, and awards ceremony',
-        levels: levelsAbove(LevelName.PARTNER),
+        levels: levelsAbove(LevelName.TITLE),
+        tooltip: 'Title sponsor may co-brand the event. Ex. HackUSU presented by YOUR COMPANY',
       },
     ],
   },
